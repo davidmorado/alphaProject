b37f12b37f0e0f822877db80ce362dcc651ff327
friediisch <friedii.schestag@googlemail.com>
2019-05-07 17:01:30 +0200

    added locally logistic regression

--- src/friedi/tf_locallylogistic.py
+++ src/friedi/tf_locallylogistic.py
@@ -12,7 +12,7 @@ y = np.linspace(-25, 25, 100)
 y = 1/(1+np.exp(-y))
 
 # Adding noise to the random linear data 
-x += np.random.uniform(-0.000001, 0.000001, 100) 
+x += np.random.uniform(-0.000001, 0.000001, 100)
 y += np.random.uniform(-0.000001, 0.000001, 100)
 
 n = len(x) # Number of data points 
@@ -25,6 +25,10 @@ plt.ylabel('y')
 plt.title("Training Data") 
 plt.show() 
 
+# convert y to tf-compatible format
+#y_comp = 1 - y
+#y = np.vstack((y, y_comp))
+#print(y.shape)
 
 x_ = tf.placeholder("float")
 X = tf.placeholder("float") 
@@ -39,29 +43,24 @@ learning_rate = 0.001
 training_epochs = 50
 
 # getting case weights for a given X for all x
-bandwidth = tf.constant(0.01, dtype="float32")
+bandwidth = tf.constant(0.1, dtype="float32")
 w = tf.exp(-tf.divide(tf.square(x_ - X), tf.square(tf.multiply(2.0, bandwidth))))
 
 # making the prediction
-y_ = tf.reduce_sum(tf.divide(1, 1 + tf.exp(-tf.add(tf.multiply(X, W), b))))
+#y_ = tf.nn.sigmoid(tf.add(tf.multiply(X, W), b))
+y_ = tf.add(tf.multiply(X, W), b)
+
+# weighing the prediction
+y_ = tf.multiply(w, y_)
 
 # Mean Squared Error Cost Function 
-#cost = tf.reduce_sum(tf.multiply(tf.pow(y_ - Y, 2), w))
-#cost = tf.reduce_mean(tf.multiply(-tf.reduce_sum(tf.multiply(Y, tf.log(y_)) + tf.multiply((1-Y), tf.log(1-y_))), w))
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=Y, logits=y_))
   
 # Gradient Descent Optimizer 
 optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
 
-# Hypothesis function
-y_pred = tf.divide(1, 1 + tf.exp(-tf.add(tf.multiply(X, W), b)))
-
-# getting case weights for a given X for all x
-#bandwidth = tf.constant(0.5, dtype="float32")
-#w = tf.exp(-tf.divide(tf.square(x_ - X), tf.square(tf.multiply(2.0, bandwidth))))
-
-# making the prediction
-#y_ = tf.reduce_sum(tf.multiply(tf.add(tf.multiply(X, W), b), w))
+# prediction for unseen point
+y_pred = 1-tf.nn.sigmoid(tf.add(tf.multiply(x_, W), b))
 
 # list for predictions
 predictions = []
@@ -69,7 +68,9 @@ beta0 = []
 beta1 = []
 
 # looping over unseen data points
-x_unseen = [-25, -20, -15, -10, 5, 0, 5, 10, 15, 20, 25]
+x_unseen = [-25, -15, 5, 5, 15, 25]
+x_unseen = np.linspace(-25, 25, 20) 
+
 for x_new in x_unseen:
 
 	# Global Variables Initializer 
@@ -85,13 +86,16 @@ for x_new in x_unseen:
 		for epoch in range(training_epochs): 
 			
 			# Feeding each data point into the optimizer using Feed Dictionary 
-			for (_x, _y) in zip(x, y): 
+			for (_x, _y) in zip(x, y):
+				_x = np.array([_x])
+				_y = np.array([_y])
+		
 				sess.run(optimizer, feed_dict = {x_: x_new, X: _x, Y: _y}) 
 			
 			# Displaying the result after every 50 epochs 
 			if (epoch + 1) % 50 == 0: 
 				# Calculating the cost a every epoch 
-				c = sess.run(cost, feed_dict = {x_: x_new, X : x, Y : y}) 
+				c = sess.run(cost, feed_dict = {x_: x_new, X: x, Y : y}) 
 				print("Epoch", (epoch + 1), ": cost =", c, "W =", sess.run(W), "b =", sess.run(b)) 
 		
 		# Storing necessary values to be used outside the Session 
@@ -102,20 +106,19 @@ for x_new in x_unseen:
 		beta0.append(b)
 
 		# Calculating the prediction for the unseen point
-		feed_dict = {x_: x_new, X: x_new}
-		yhat = sess.run(y_, feed_dict) 
+		feed_dict = {x_: x_new, X: x}
+		yhat = sess.run(y_pred, feed_dict)
 		predictions.append(yhat)
 
 		# writing session graph
 		writer = tf.summary.FileWriter("log/", sess.graph)
 
-	#print("Training cost =", training_cost, "Weight =", weight, "bias =", bias, '\n')
 
 print(predictions)
 
 # Plotting the Results
 
-#plt.plot(x, y, 'ro', label ='Original data') 
+plt.plot(x, y, 'ro', label ='Original data', alpha=0.5) 
 plt.scatter(x_unseen, predictions, label ='Predictions', c='blue', marker='x')
 
 plt.title('Locally Linear Regression Result') 
