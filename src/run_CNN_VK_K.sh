a6ff418082df680d1a440a69707763c3d86f5db2
schestag <schestag@master.ismll.de>
2019-05-20 16:54:32 +0200

    added shell script to execute CNN with varkeys on GPU partition

---
+++ src/run_CNN_VK_K.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=CNN_VK_K
+#SBATCH --output=test%j.log
+#SBATCH --error=test%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=schestag@uni-hildesheim.de
+
+
+# ## FOR GPU USE:
+#SBATCH --partition=GPU
+#SBATCH --gres=gpu:1
+source activate test
+
+## Run the script
+# #SBATCH --export=NONE
+#source /home/schestag/miniconda3/bin/activate test
+#srun /home/schestag/miniconda3/envs/test/bin/python test.py
+srun python CNN+Varkeys+Keras.py
