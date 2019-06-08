66975ddfa415dbe7546cab2e8c06db1accbe8c82
friediisch <friedii.schestag@googlemail.com>
2019-06-08 12:03:20 +0200

    changed Optimizer to Adadelta

---
+++ src/model_v4_Adadelta/fit_evaluate.py
@@ -0,0 +1,35 @@
+import keras
+import tensorflow as tf
+
+def fit_evaluate(model, x_train, y_train, x_test,  y_test, batch_size, epochs, logstring):
+
+    model.compile(loss=keras.losses.categorical_crossentropy,
+                optimizer = keras.optimizers.Adadelta(lr=1.0, rho=0.95, epsilon=None, decay=0.0),
+                metrics=['accuracy'])
+
+    tbCallBack = keras.callbacks.TensorBoard(log_dir=logstring, histogram_freq=0,  
+          write_graph=True, write_images=True)
+
+    early_stopping = keras.callbacks.EarlyStopping(monitor='val_loss',
+        min_delta=0, patience=10, verbose=2, mode='auto', restore_best_weights=True)
+
+    history = model.fit(x_train, y_train,
+            batch_size=batch_size,
+            epochs=epochs,
+            verbose=1,
+            validation_data=(x_test, y_test), 
+            callbacks = [tbCallBack, early_stopping])
+
+    # get varkeys in the final layers
+    #memory = model.layers[-1].get_memory()
+    #memory = tf.Session().run(memory)
+    memory = model.get_weights()[-1]
+    
+    val_acc = history.history['val_acc']
+    acc = history.history['acc']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    
+    scores = model.evaluate(x_test, y_test)
+    print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+    return val_acc, acc, loss, val_loss, scores, memory
---
+++ src/model_v4_Adadelta/myparallelizer.py
@@ -0,0 +1,42 @@
+# learning rates
+# not applicable since ADADELTA DEFAULT LEARN RATE IS USED
+# lrs = [1e-6, 1e-5, 1e-4, 1e-3]
+# bandwith sizes
+bws = [1e-2, 1e-1, 1, 1e1, 1e2]
+# number of keys per class
+kpcs = [1, 1e1, 1e2, 1e3]
+# embedding sizes
+ess = [int(3000/1e3), int(3000/1e2), int(3000/1e1)]
+# percentage of training data used
+tps = [1, 0.5, 0.25, 0.125]
+
+##########
+# bandwith sizes
+bws = [1]
+# number of keys per class
+kpcs = [1e2]
+# embedding sizes
+ess = [int(3000/1e2)]
+# percentage of training data used
+tps = [0.5]
+
+
+import os
+
+# creates folders
+folders = ['models', 'gridresults', 'tb_logs', 'errs', 'logs']
+for f in folders:
+    try:
+        os.makedirs(f)
+    except OSError:
+        pass
+
+for bw in bws:
+	bw = float(bw)
+	for kpc in kpcs:
+		kpc = int(kpc)
+		for es in ess:
+			es = int(es)
+			for tp in tps:
+				tp = float(tp)
+				os.system(F"sbatch template.sh {bw} {kpc} {es} {tp}")
