93489ef706ae94dbc9c32192f466b63f5e0f2631
friediisch <friedemann.schestag@outlook.com>
2019-06-12 10:31:47 +0200

    new results in proper dict form

--- src/model_v6_NoCache/myparallelizer.py
+++ src/model_v6_NoCache/myparallelizer.py
@@ -15,7 +15,7 @@ tps = [1, 0.5, 0.25, 0.125]
 ##########
 # TEST MODE
 # don't forget to set partition=TEST in template.sh
-testing = True
+testing = False
 if testing:
 	# embedding sizes
 	ess = [300]
