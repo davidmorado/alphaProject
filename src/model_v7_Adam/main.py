b1908da3aa3843b1f79639c6a2964b1187782749
friediisch <friedii.schestag@googlemail.com>
2019-06-18 15:30:04 +0200

    bla

--- src/model_v7_Adam/main.py
+++ src/model_v7_Adam/main.py
@@ -66,7 +66,7 @@ model = CNN_VK(
     n_keys_per_class=n_keys_per_class, 
     bandwidth=bandwidth)
 
-modelpath = F"Adadelta_bw={hp_dict['bandwidth']}_kpc={hp_dict['n_keys_per_class']}_es={hp_dict['embedding_dim']}_tp={hp_dict['train_percentage']}"
+modelpath = F"Adam_lr={learning_rate}_bw={hp_dict['bandwidth']}_kpc={hp_dict['n_keys_per_class']}_es={hp_dict['embedding_dim']}_tp={hp_dict['train_percentage']}"
 
 metrics_dict, memory = fit_evaluate(model, x_train_, y_train_, x_test, y_test, batch_size, epochs, lr=learning_rate, logstring=F'tb_logs/{modelpath}')
 
