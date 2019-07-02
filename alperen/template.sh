5e53f16712e3b4397369b1c503510fca513c7120
David Obando Paniagua <dobando.paniagua@Davids-MacBook-Pro-Party.local>
2019-07-02 19:55:55 +0200

    Sbatch File Add

---
+++ alperen/template.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=test_alperen
+#SBATCH --output=test%j.log
+#SBATCH --error=test%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=dedeoglu@uni-hildesheim.de
+
+
+
+# ## FOR GPU USE:
+#SBATCH --partition=STUD
+#SBATCH --gres=gpu:1
+source activate projectGPU
+
+
+## Run the script
+srun python CNN+Varkeys+Keras_alperen.py
