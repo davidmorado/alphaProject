a0f4568d9927ed625983274f9befd5b342c474f7
friediisch <friedii.schestag@googlemail.com>
2019-10-16 00:58:26 +0200

    gridsearch SC

--- src/model_v8_SimpleCache/fit_evaluate.py
+++ src/model_v8_SimpleCache/fit_evaluate.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 import numpy as np
 import pdb
 
-def fit_evaluate(model, x_train, y_train, x_test,  y_test, batch_size, epochs, lr, logstring, cache_fraction=1, lmbd=1):
+def fit_evaluate(model, x_train, y_train, x_test,  y_test, batch_size, epochs, lr, logstring, theta=0.5, cache_fraction=0.1, lmbd=0.5):
 
     model.compile(loss=keras.losses.categorical_crossentropy,
                   optimizer = keras.optimizers.Adam(lr=lr, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False),
@@ -22,108 +22,64 @@ def fit_evaluate(model, x_train, y_train, x_test,  y_test, batch_size, epochs, l
             verbose=1,
             validation_data=(x_test, y_test), 
             callbacks = [tbCallBack, early_stopping])
-
-    # print('model.layers')
-    # for i in range(len(model.layers)):
-    #     print(i, model.layers[i])
-    #     print(i, np.array(model.layers[i].get_weights()).shape)
-    #     #print(i, np.array(model.layers[i].get_weights()))
-    # produces:
-    # 0 <keras.layers.convolutional.Conv2D object at 0x1332ee080>
-    # 1 <keras.layers.core.Activation object at 0x133477cf8>
-    # 2 <keras.layers.convolutional.Conv2D object at 0x133477208>
-    # 3 <keras.layers.core.Activation object at 0x1334a5b38>
-    # 4 <keras.layers.pooling.MaxPooling2D object at 0x1334b6b38>
-    # 5 <keras.layers.core.Dropout object at 0x1334a5f60>
-    # 6 <keras.layers.convolutional.Conv2D object at 0x1334a5eb8>
-    # 7 <keras.layers.core.Activation object at 0x1334cbc18>
-    # 8 <keras.layers.convolutional.Conv2D object at 0x1334cb1d0>
-    # 9 <keras.layers.core.Activation object at 0x14020de80>
-    # 10 <keras.layers.pooling.MaxPooling2D object at 0x14020dd68>
-    # 11 <keras.layers.core.Dropout object at 0x14021e470>
-    # 12 <keras.layers.core.Flatten object at 0x14021e048>
-    # 13 <keras.layers.core.Dense object at 0x116a54400>
-    # 14 <keras.layers.core.Activation object at 0x116a7a8d0>
-    # 15 <keras.layers.core.Dropout object at 0x116a7a5c0>
-    # 16 <keras.layers.core.Dense object at 0x116a7af98>
-    # 17 <keras.layers.core.Activation object at 0x116a8e5f8>
-    # 18 <keras.layers.normalization.BatchNormalization object at 0x116a8ec50>
-    # 19 <keras.layers.core.Dense object at 0x116a8edd8>
-    # 20 <keras.layers.core.Activation object at 0x117096da0>
     
     # SIMPLE CACHE
     num_classes = 10
     mem_layers = [14, 17, 20]
-
+    mem_layers = [17]
+    
     output_list = []
     for i in range(len(mem_layers)):
         output_list.append(model.layers[mem_layers[i]].output)
 
-    # extra memory model:
-    memory_extractor = Model(inputs=model.input, outputs=output_list)
+    # Specify memory
+    mem = Model(inputs=model.input, outputs=output_list )
 
-    # randomly order the training data indices and select only those up to the selected percentage: 
-    cache_indices = np.random.permutation(np.arange(x_train.shape[0]))[:int(x_train.shape[0]*cache_fraction)]
+    # subsample data points for memory module (further subsampling from potentially subsampled training data)
+    idx = np.random.permutation(np.arange(x_train.shape[0]))[:int(x_train.shape[0]*cache_fraction)]
 
-    x_train_sub = x_train[cache_indices, :, :, :]
-    y_train_sub = y_train[cache_indices, :]
+    x_train_in_mem = x_train[idx, :, :, :]
+    y_train_in_mem = y_train[idx, :]
 
-    # replace x/y_test with x/y_train_sub should return 100% accuracy
-    if True:
-        x_test = x_train_sub
-        y_test = y_train_sub
-
+    # assert 100% accuracy for x_test == x_train_in_mem
     if False:
-        x_test = x_train_sub[:-1, :, :, :]
-        y_test = y_train_sub[:-1, :]
+        x_test = x_train_in_mem[:-1, :, :]
+        y_test = y_train_in_mem[:-1, :]
 
-    print('shape')
-    print(x_test.shape)
-    print(y_test.shape)
-
-    # Pass subset items through memory to extract the activations for each memory point
-    memkeys_list = memory_extractor.predict(x_train_sub)
-    if len(mem_layers) == 1: memkeys_list = memkeys_list[None,:]
-    mem_keys = np.reshape(memkeys_list[0],(x_train_sub.shape[0],-1))
+    # Memory keys
+    memkeys_list = mem.predict(x_train_in_mem)
+    mem_keys = np.reshape(memkeys_list[0],(x_train_in_mem.shape[0],-1))
     for i in range(len(mem_layers)-1):
-        mem_keys = np.concatenate((mem_keys, np.reshape(memkeys_list[i+1],(x_train_sub.shape[0],-1))),axis=1)
+        mem_keys = np.concatenate((mem_keys, np.reshape(memkeys_list[i+1],(x_train_in_mem.shape[0],-1))),axis=1)
 
     # Memory values
-    mem_vals = y_train_sub
+    mem_vals = y_train_in_mem
 
-    # Pass test items through memory to extract the activations for each memory point
-    testmem_list = memory_extractor.predict(x_test)
-    if len(mem_layers) == 1: testmem_list = testmem_list[None,:] 
-    test_mem = np.reshape(testmem_list[0],(x_test.shape[0],-1)) # get first activation
+    # Pass items thru memory
+    testmem_list = mem.predict(x_test)
+    test_mem = np.reshape(testmem_list[0],(x_test.shape[0],-1))
     for i in range(len(mem_layers)-1):
         test_mem = np.concatenate((test_mem, np.reshape(testmem_list[i+1],(x_test.shape[0],-1))),axis=1)
 
     # Normalize keys and query
-    key = mem_keys / np.sqrt(np.tile(np.sum(mem_keys**2, axis=1, keepdims=1), (1, mem_keys.shape[1])))
-    query = test_mem / np.sqrt(np.tile(np.sum(test_mem**2, axis=1, keepdims=1), (1, test_mem.shape[1])))
+    query = test_mem / np.sqrt( np.tile(np.sum(test_mem**2, axis=1, keepdims=1),(1,test_mem.shape[1])) )
+    key = mem_keys / np.sqrt( np.tile(np.sum(mem_keys**2, axis=1, keepdims=1),(1,mem_keys.shape[1])) )
 
-    theta = 0.6
-    similarities = np.exp(theta * (query@key.T)) # Eq(1)
-    similarities_ = np.repeat(np.expand_dims(similarities, axis=2), num_classes, axis=2)
-    mem_vals_ = np.repeat(np.expand_dims(mem_vals, axis=1), x_test.shape[0], axis=1)
-    p_mem_enum = np.sum(np.multiply(similarities_, mem_vals_), axis=1, keepdims=False) # Eq(2) enumerator
-    p_mem_denom = np.repeat(np.expand_dims(np.sum(similarities, axis=0), axis=1), num_classes, axis=1) #Eq(2) denominator
-    # p_mem_enum = np.matmul(similarities, mem_vals) # Eq(2) enumerator
-    # p_mem_denom = np.repeat(np.sum(similarities, axis=1, keepdims=True), num_classes, axis=1) #Eq(2) denominator
-    p_mem = p_mem_enum / p_mem_denom #Eq(2)
+    similarities = np.exp(theta * np.dot(query, key.T) )
+    p_mem = np.matmul(similarities, mem_vals)
+    p_mem = p_mem / np.repeat(np.sum(p_mem, axis=1, keepdims=True), num_classes, axis=1)
 
     p_model = model.predict(x_test)
 
-    # lmbd is passed 
-    lmbd = 0.5
     p_combined = (1.0-lmbd) * p_model + lmbd * p_mem
     pred_combined = np.argmax(p_combined, axis=1)
     y_test_int = np.argmax(y_test, axis=1)
-    print('This should return 100% Accuracy when using x_test = x_train_sub:')
     test_acc = np.mean(pred_combined==y_test_int)
+    mem_only_acc = np.mean(np.argmax(p_mem, axis=1)==y_test_int)
 
     print('Mem. shape:', mem_keys.shape)
-    print('Mem. accuracy:', test_acc)
+    print('Mem. accuracy:', mem_only_acc)
+    print('Mem. + model accuracy:', test_acc)
 
     mem_loss, mem_accuracy = model.evaluate(x_test, y_test, verbose=1)
     print('model loss:', mem_loss)
@@ -134,20 +90,20 @@ def fit_evaluate(model, x_train, y_train, x_test,  y_test, batch_size, epochs, l
     loss = history.history['loss']
     val_loss = history.history['val_loss']
 
-    pdb.set_trace()
-
     metrics_dict = {
-        'acc': acc,
-        'val_acc': val_acc,
-        'loss': loss,
-        'val_loss': val_loss,
-        'mem_loss': mem_loss,
-        'mem_acc': mem_accuracy
+        'model_only_acc_train': acc,
+        'model_only_acc_val': val_acc,
+        'model_only_loss_train': loss,
+        'model_only_loss_val': val_loss,
+        'model_loss_test': mem_loss,
+        'model_acc_test': mem_accuracy,
+        'mem_only_acc_test': mem_only_acc,
+        'comb_acc_test': test_acc
     }
     
     scores = model.evaluate(x_test, y_test)
     print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
-
+    pdb.set_trace()
     
     return metrics_dict, key
---
+++ src/model_v8_SimpleCache/gridsearch_SC.py
@@ -0,0 +1,112 @@
+import os
+import sys
+from CNN import CNN
+from data_loader import get_dataset
+from batch import batch
+import tensorflow as tf
+import numpy as np
+from keras import Model
+from memory_predictions import memory_predictions
+import pickle
+
+# creates folders
+folders = ['models', 'gridresults', 'tb_logs', 'errs', 'logs']
+for f in folders:
+    try:
+        os.makedirs(f)
+    except OSError:
+        pass
+
+x_train, x_val, x_test, y_train, y_val, y_test = get_dataset('cifar10', normalize=True)
+num_categories = y_train.shape[1]
+input_shape = x_train.shape[1:]
+
+print('Data has been loaded successfully!')
+
+# Hyperparameters:
+batch_size = 2
+epochs = 1
+learning_rate = 0.001
+embedding_dim = 100
+
+print('Hyperparameters are set!')
+
+model = CNN(
+    num_categories,
+    input_shape=input_shape, 
+    layers=[32, 64, 512], 
+    embedding_dim=embedding_dim)
+
+
+# Model graph
+x = tf.placeholder(tf.float32, shape=(None, 32, 32, 3), name='input_x')
+y = tf.placeholder(tf.float32, shape=(None, 10), name='output_y')
+pred = model(x)
+cost = tf.reduce_mean(tf.losses.softmax_cross_entropy(logits=pred, onehot_labels=y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32), name='accuracy')
+
+print('Graph has been created!')
+
+# cfg = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+# tg = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+# lg = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+cfg = [0.1]
+tg = [10]
+lg = [0.1]
+metrics = ['train_acc', 'val_acc', 'test_acc', 'train_loss', 'val_loss', 'test_loss', 
+            'mem_acc_val', 'comb_acc_val', 'mem_acc_test', 'comb_acc_test']
+
+metrics_dict = {}
+for cf in cfg:
+    for t in tg:
+        for l in lg:
+            metrics_dict[(cf, t, l)] = {}
+            for m in metrics:
+                metrics_dict[(cf, t, l)][m] = []
+
+
+with tf.Session() as sess:
+
+    sess.run(tf.global_variables_initializer())
+
+    for epoch in range(epochs):
+        idx = np.random.randint(0, x_train.shape[0], x_train.shape[0])
+        num_batches = int(x_train.shape[0]/batch_size)
+        batch_idxs = np.array_split(idx, num_batches)
+
+        for bidx in batch_idxs:
+
+            # train
+            sess.run(optimizer, feed_dict = {x: x_train[idx], y: y_train[idx]})
+
+            print('Training step taken.')
+
+            # get accuracy
+            train_acc = sess.run(accuracy, feed_dict = {x: x_train, y: y_train})
+            val_acc = sess.run(accuracy, feed_dict = {x: x_val, y: y_val})
+            test_acc = sess.run(accuracy, feed_dict = {x: x_test, y: y_test})
+
+            # get loss
+            train_loss = sess.run(cost, feed_dict = {x: x_train, y: y_train})
+            val_loss = sess.run(cost, feed_dict = {x: x_val, y: y_val})
+            test_loss = sess.run(cost, feed_dict = {x: x_test, y: y_test})
+
+            print('Standard Metrics recorded.')
+
+            for cf in cfg:
+                for t in tg:
+                    for l in lg:
+                        try:
+                            mem_acc_val, comb_acc_val = memory_predictions(model, x_train, x_val, y_train, y_val, num_categories, cf, t, l)
+                            mem_acc_test, comb_acc_test = memory_predictions(model, x_train, x_test, y_train, y_test, num_categories, cf, t, l)
+                        except:
+                            mem_acc_val, comb_acc_val, mem_acc_test, comb_acc_test = ('OF', 'OF', 'OF', 'OF')
+                        for m in metrics:
+                            metrics_dict[(cf, t, l)][m].append(eval(m))
+            
+            with open('results.pickle', 'wb') as f:
+                pickle.dump(metrics_dict, f)
+
+            print('Memory Metrics recorded and all metrics saved.')
