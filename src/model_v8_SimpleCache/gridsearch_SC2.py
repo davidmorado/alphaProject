cf6055cece8173a58ab24f771455cabe2a4eb809
friediisch <friedii.schestag@googlemail.com>
2019-10-16 15:47:09 +0200

    bla

--- src/model_v8_SimpleCache/gridsearch_SC2.py
+++ src/model_v8_SimpleCache/gridsearch_SC2.py
@@ -61,8 +61,6 @@ val_acc = history.history['val_acc']
 train_loss = history.history['loss']
 val_loss = history.history['val_loss']
 
-pdb.set_trace()
-
 cfg = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
 tg = [10, 20, 30, 40, 50, 60, 70, 80, 90]
 lg = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
